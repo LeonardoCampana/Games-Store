ca01ea1c1f33ac1ac3ff98eb30adc1c14d01baea
Leonardo Campana <leonardobcampana@gmail.com>
2024-07-03 19:11:34 -0300

    primeriro commit

---
+++ GamesStore/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamesStore.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Autentication/Login.cshtml");
+        }
+    }
+}
