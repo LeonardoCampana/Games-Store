a56aef56196ab352ee430f9c3c5b8c6ab402e74a
Leonardo Campana <leonardobcampana@gmail.com>
2024-07-30 22:22:03 -0300

    add shopcart

--- GamesStore/obj/Debug/net8.0/GamesStore.AssemblyInfo.cs
+++ GamesStore/obj/Debug/net8.0/GamesStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamesStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbcff94b8aab050027b3e7e20ea88432674d1c70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006fb5339fa2e202d07f13e7e433e6f3f35d640f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamesStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamesStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
